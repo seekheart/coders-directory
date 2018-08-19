5196a233642e2f09a14b2e3f71e316a7df16cbd3
Mike Tung <miketung2013@gmail.com>
2018-08-19 09:38:26 -0400

    #2 updated layout by removing side nav because it didn't serve a good enough purpose.

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatGridListModule, MatIconModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatSidenavModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule
   ],
   exports: [
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatSidenavModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule
   ]
 })
 export class MaterialModule {
